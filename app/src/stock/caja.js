99aa3a36ecc17544bca9c8e85ba316cf10f5a2d9
Matias035 <48734377+Matias035@users.noreply.github.com>
2022-05-22 12:46:37 -0300

    Se comienza con la caja
    Primero pasos de caja.

---
+++ app/src/stock/caja.js
@@ -0,0 +1,15 @@
+import producto from "./producto";
+import Stock from "./stock";
+
+var instanceStock = new Stock();
+
+instanceStock.consultaProductos();
+const listaDeProductos = instanceStock.obtenerListaProductos();
+
+function rellenarProductos() {
+    const selectProductos = document.getElementById("select_nuevaVenta");
+    for (let index = 0; index < listaDeProductos.length; index++) {
+        const nomProd = listaDeProductos[index].nombre;
+        var snproducto;  
+    } 
+}
