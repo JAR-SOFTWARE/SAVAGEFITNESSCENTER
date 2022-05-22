40a7264d828021156525575d51cea1b911fba33b
Matias035 <48734377+Matias035@users.noreply.github.com>
2022-05-22 12:48:33 -0300

    Update caja.js

--- app/src/stock/caja.js
+++ app/src/stock/caja.js
@@ -5,7 +5,7 @@ var instanceStock = new Stock();
 
 instanceStock.consultaProductos();
 const listaDeProductos = instanceStock.obtenerListaProductos();
-
+/* <-- Funcion de rellenar selec de productos en modal -->*/
 function rellenarProductos() {
     const selectProductos = document.getElementById("select_nuevaVenta");
     for (let index = 0; index < listaDeProductos.length; index++) {
