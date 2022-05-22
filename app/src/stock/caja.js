1f4e36e5da98224cace3a39b45f9836a4c25191a
Matias035 <48734377+Matias035@users.noreply.github.com>
2022-05-22 13:01:05 -0300

    Update caja.js

--- app/src/stock/caja.js
+++ app/src/stock/caja.js
@@ -5,11 +5,15 @@ var instanceStock = new Stock();
 
 instanceStock.consultaProductos();
 const listaDeProductos = instanceStock.obtenerListaProductos();
-/* <-- Funcion de rellenar selec de productos en modal -->*/
+/* <-- Funcion de rellenar selec de productos en modal --> */
 function rellenarProductos() {
     const selectProductos = document.getElementById("select_nuevaVenta");
     for (let index = 0; index < listaDeProductos.length; index++) {
         const nomProd = listaDeProductos[index].nombre;
         var snproducto;  
     } 
+}
+
+function procesarVenta(producto) {
+    
 }
