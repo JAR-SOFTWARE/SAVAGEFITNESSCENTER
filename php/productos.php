379d9145e63b4843f478ff7dac34f82f71992d00
jromero764 <85945162+jromero764@users.noreply.github.com>
2022-05-24 14:17:33 -0300

    Actualizacion
    Actualizacion

--- php/productos.php
+++ php/productos.php
@@ -40,7 +40,7 @@ case '3'://MODIFICACION DE PRODUCTO
         $row=$resultado->fetch_array();
         echo json_encode($row[0]);        
     }
-
+    break;
 case '4'://VENTA DE PRODUCTO
     $fecha=$_POST['fecha_n'];
     $nombre=$_POST['nombre'];
@@ -54,6 +54,7 @@ case '4'://VENTA DE PRODUCTO
         $row=$resultado->fetch_array();
         echo json_encode($row[0]);        
     }
+    break;
 case '5'://CONSULTA DE VENTAS 
         $fecha=$_POST['fecha'];
         $consulta="SELECT * FROM vVentasGeneral WHERE Fecha='$fecha'";
@@ -68,4 +69,14 @@ case '5'://CONSULTA DE VENTAS
             echo json_encode('No tiene dato');
         };
         break;
+case '6'://BAJA DE PRODUCTO
+            $producto=$_POST['producto'];
+            $consulta="SELECT bProducto('$producto')";
+            $select= mysqli_query($conexion,$consulta);
+            if($resultado){
+                $row=$resultado->fetch_array();
+                echo json_encode($row[0]);        
+            }
+            
+            break;        
 }
