e9a0dd92c18b4a4e3e59a92b75689ef08cead4a6
Matias035 <matiasangel2006@gmail.com>
2022-05-25 23:16:40 -0300

    Se realiza el modulo de compra
    Se realiza modulo de compra

--- app/src/stock/caja.js
+++ app/src/stock/caja.js
@@ -15,18 +15,32 @@ $( document ).ready(function() {
 /* <-- Funcion de rellenar selec de productos en modal --> */
 function rellenarProductos() {
     const selectProductos = document.getElementById("select_nuevaVenta");
+    const selectProductosCompra = document.getElementById('select_nuevaCompra')
     for (let index = 0; index < listaDeProductos.length; index++) {
         var nomProd = listaDeProductos[index].nombre;
         selectProductos.innerHTML = selectProductos.innerHTML +'<option>'+nomProd+'</option>'; 
+        selectProductosCompra.innerHTML = selectProductosCompra.innerHTML +'<option>'+nomProd+'</option>';
     } 
 }
 
 /* <-- Funcion de con la cual obtengo el producto seleccionado --> */
 document.getElementById('btnreg-venta').onclick = function(){buscarProducto($('#select_nuevaVenta').val())}
+document.getElementById('btnreg-compra').onclick = function(){buscarProductoCompra($('#select_nuevaVenta').val())}
+
 function buscarProducto(nombreSeleccionado) {
     for (let index = 0; index < listaDeProductos.length; index++) {
         if (listaDeProductos[index].nombre == nombreSeleccionado) {
             return procesarVenta(listaDeProductos[index],$('#cantidad-producto').val()); 
+        }
+    }
+    console.log('El producto no se encuentra en Stock');
+}
+
+
+function buscarProductoCompra(nombreSeleccionado) {
+    for (let index = 0; index < listaDeProductos.length; index++) {
+        if (listaDeProductos[index].nombre == nombreSeleccionado) {
+            return procesarCompra(listaDeProductos[index],$('#cantidad-producto-compra').val()); 
         }
     }
     console.log('El producto no se encuentra en Stock');
@@ -46,12 +60,51 @@ function procesarVenta(productoAprocesar,cantidad) {
     }
 }
 
+function procesarCompra(productoAprocesar,cantidad) {
+    if (productoAprocesar.cantidad > 0) {      
+        if (productoAprocesar.cantidad - cantidad >0) {
+            productoAprocesar.cantidad = productoAprocesar.cantidad - cantidad; 
+            altaCompra(productoAprocesar,cantidad);
+        }else{
+            console.log('No se encuentra la suficiente cantidad del producto')
+        }
+    }else{
+        console.log('No hay stock del producto');
+    }
+}
+
 function altaVenta(productoAprocesar,cantidad) {
      //Este fragmento de codigo realiza la venta del producto. 
      var fechaDehoy = new Date();
      var fecha = fechaDehoy.getDate() + '-0' + ( fechaDehoy.getMonth() + 1 ) + '-' + fechaDehoy.getFullYear();
-     productoAprocesar.ventaDeProducto(fecha,'Productos',productoAprocesar.nombre,productoAprocesar.precioVenta,cantidad);
-     cVentas( fechaDehoy.getFullYear()+ '-0' + ( fechaDehoy.getMonth() + 1 ) + '-' + fechaDehoy.getDate());
+     var div = document.getElementById('div_footer_ventas');
+     if (cantidad <= 0) {
+         div.innerHTML = '<div class="alert alert-danger" role="alert">No se puede realizar la venta con cantidad menor o igual a 0 </div>';
+     }else{
+        productoAprocesar.precioVenta = productoAprocesar.precioVenta * cantidad;
+        productoAprocesar.ventaDeProducto(fecha,'Productos',productoAprocesar.nombre,productoAprocesar.precioVenta,cantidad);
+        cVentas( fechaDehoy.getFullYear()+ '-0' + ( fechaDehoy.getMonth() + 1 ) + '-' + fechaDehoy.getDate());
+        setTimeout(function () {
+            div.innerHTML = '<div class="alert alert-success" role="alert">Se ha procesado la venta correctamente</div>';
+        },1000);
+     }
+}
+
+function altaCompra(productoAprocesar,cantidad) {
+    //Este fragmento de codigo realiza la venta del producto. 
+    var fechaDehoy = new Date();
+    var fecha = fechaDehoy.getDate() + '-0' + ( fechaDehoy.getMonth() + 1 ) + '-' + fechaDehoy.getFullYear();
+    var div = document.getElementById('div_footer_compras');
+    if (cantidad <= 0) {
+        div.innerHTML = '<div class="alert alert-danger" role="alert">No se puede realizar la compra con cantidad menor o igual a 0 </div>';
+    }else{
+       productoAprocesar.precioVenta = productoAprocesar.precioVenta * cantidad;
+       productoAprocesar.ventaDeProducto(fecha,'Compra',productoAprocesar.nombre,productoAprocesar.precioVenta,cantidad);
+       cVentas( fechaDehoy.getFullYear()+ '-0' + ( fechaDehoy.getMonth() + 1 ) + '-' + fechaDehoy.getDate());
+       setTimeout(function () {
+           div.innerHTML = '<div class="alert alert-success" role="alert">Se ha procesado la compra correctamente</div>';
+       },1000);
+    }
 }
 
 /*SECCION LISTADO POR FECHA */
@@ -86,16 +139,29 @@ function cVentas(fecha) {
         var i=0;
         tbody_producto.innerHTML="";
         var total = 0;
+        var totalCompra = 0;
         for(var i=0; i<js.length; i++){
-            tbody_producto.innerHTML+=`
-            <tr>
-              <td>${js[i].Tipo}</td>
-              <td>${js[i].Descripcion}</td>
-              <td>${js[i].Efectivo}</td>
-           </tr>
-            `;
-            total += parseInt(js[i].Efectivo);
-            document.getElementById("ventasTotales").innerHTML = '$'+total+'.00'
+            if (js[i].Tipo =='Compra') {
+                tbody_producto.innerHTML+=`
+                <tr>
+                <td>${js[i].Tipo}</td>
+                <td>${js[i].Descripcion}</td>
+                <td>${js[i].Efectivo}</td>
+                </tr>
+                `;
+                totalCompra += parseInt(js[i].Efectivo);
+                document.getElementById("comprasTotales").innerHTML = '$'+totalCompra+'.00'
+            }else{
+                tbody_producto.innerHTML+=`
+                <tr>
+                <td>${js[i].Tipo}</td>
+                <td>${js[i].Descripcion}</td>
+                <td>${js[i].Efectivo}</td>
+                </tr>
+                `;
+                total += parseInt(js[i].Efectivo);
+                document.getElementById("ventasTotales").innerHTML = '$'+total+'.00';
+            }
           }
     }).fail( function( jqXHR, textStatus, errorThrown ) {
     
@@ -142,5 +208,6 @@ $( document ).ready(function() {
     if(mes<10)
       mes='0'+mes //agrega cero si el menor de 10
     document.getElementById("Fecha_cVenta").value = ano+"-"+mes+"-"+dia;
-    cVentas(fecha.getFullYear()+ '-0' + ( fecha.getMonth() + 1 ) + '-' + fecha.getDate());
+    cVentas(ano+"-"+mes+"-"+dia);
  });
+
